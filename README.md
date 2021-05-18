94a4a07e5dd7e6ef3a7db3fce47ee3fb0b14c232
Li Jiayu <jiayu.li@shopee.com>
2021-05-18 17:50:24 +0800

    Write unit tests

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 
 ## Start program
 
-1. In the `kubernetes` directory, run `go build`.
+1. In the `kubernetes-trial` directory, run `go build`.
 1. Run `./k8s-trial` to start running the program.
 
 ## Instructions after launching the program
 
 In the line asking for `Task (view, create, or delete): `, type in a task. Valid tasks are `view`, `create`, `delete`, 
 and `exit`. Then follow the tips as provided in the stdout to provide further input.
+
+## Unit tests
+
+In `kubernetes-trial` directory, run `go test`, and wait. It will take about 2.5 minutes to finish the tests. Do make 
+sure that before running the tests, there is no deployment called "kubernetes-bootcamp" running (if so, run `kubectl 
+delete deployment kubernetes-bootcamp` and wait for a while until `kubectl get pod` would show no pods running 
+deployment `kubernetes-bootcamp`.
---
+++ unit_test.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"log"
+	"os"
+	"strings"
+	"testing"
+	"time"
+)
+
+func Test(t *testing.T) {
+	clientset := connectToK8s()
+
+	t.Run("One iteration", func(t *testing.T) {
+		deploymentName := "kubernetes-bootcamp"
+		var newPods map[string]bool
+
+		t.Run("Create", func(t *testing.T) {
+			newPods = createUnitTest(t, clientset, deploymentName)
+		})
+
+		// Test log: https://stackoverflow.com/questions/44119951/how-to-check-a-log-output-in-go-test
+		t.Run("View", func(t *testing.T) {
+			viewUnitTest(t, clientset, newPods)
+		})
+
+		t.Run("Delete", func(t *testing.T) {
+			deletedPods := deleteUnitTest(t, clientset, deploymentName)
+			checkIsValidDelete(t, deletedPods, newPods)
+		})
+	})
+}
+
+func checkIsValidDelete(t *testing.T, deletedPods map[string]bool, newPods map[string]bool) {
+	t.Logf("Deleted pods are %v", deletedPods)
+	for k, _ := range newPods {
+		if _, ok := deletedPods[k]; !ok {
+			t.Errorf("The pod %v created but not deleted.", k)
+		}
+	}
+	for k, _ := range deletedPods {
+		if _, ok := newPods[k]; !ok {
+			t.Errorf("The pod %v deleted but was not created in test.", k)
+		}
+	}
+}
+
+func viewUnitTest(t *testing.T, clientset *kubernetes.Clientset, newPods map[string]bool) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+
+	getPodsOfNamespace(clientset, "default")
+	time.Sleep(10 * time.Second)
+
+	logs := strings.Split(buf.String(), "\n")
+	pods := make(map[string]bool)
+	for _, l := range logs {
+		if l == "" {
+			break
+		}
+		index := strings.LastIndex(l, ":")
+		pods[strings.Trim(l[(index+1):], " \n")] = true
+	}
+
+	t.Logf("New pods are %v", newPods)
+	t.Logf("Pods are %v", pods)
+	for k, _ := range newPods {
+		if _, ok := pods[k]; !ok {
+			t.Errorf("Failed to get the pod %v", k)
+		}
+	}
+}
+
+func createUnitTest(t *testing.T, clientset *kubernetes.Clientset, deploymentName string) map[string]bool {
+	result := make(map[string]bool)
+	originalDeployments := getDeploymentsOfDefaultNamespace(t, clientset)
+	existingDeployments := convertDeploymentListToMapOfName(originalDeployments)
+	originalPods := getPodsOfDefaultNamespace(t, clientset)
+	existingPods := convertPodListToMapOfName(originalPods)
+
+	createSampleDeployment(clientset, "demo", deploymentName)
+	time.Sleep(40*time.Second)
+
+	deployments := getDeploymentsOfDefaultNamespace(t, clientset)
+	numOfNewDeployments := len(deployments) - len(originalDeployments)
+	if numOfNewDeployments != 1 {
+		t.Errorf("Want to create one deployment, but failed. Get %d new deployments.", numOfNewDeployments)
+	}
+	pods := getPodsOfDefaultNamespace(t, clientset)
+	numOfNewPods := len(pods) - len(originalPods)
+	if numOfNewPods != 4 {
+		t.Errorf("Want to create four replicas, but failed. Get %d new pods.", numOfNewPods)
+	}
+
+	for _, d := range deployments {
+		if _, ok := existingDeployments[d.Name]; !ok {
+			t.Logf("The new deployment is: %s", d.Name)
+			if d.Name != deploymentName {
+				t.Errorf("The newly created deployment is not of desired name")
+			}
+		}
+	}
+	for _, p := range pods {
+		if _, ok := existingPods[p.Name]; !ok {
+			t.Logf("New pod: %s", p.Name)
+			result[p.Name] = true
+			if !strings.HasPrefix(p.Name, deploymentName) {
+				t.Errorf("The newly created pod is not of desired name")
+			}
+		}
+	}
+	return result
+}
+
+func createSampleDeployment(clientset *kubernetes.Clientset, appName string, deploymentName string) {
+	launchK8sDeployment(
+		clientset,
+		"default",
+		appName,
+		deploymentName,
+		"kubernetes-bootcamp",
+		"gcr.io/google-samples/kubernetes-bootcamp:appsv1")
+}
+
+func getDeploymentsOfDefaultNamespace(t *testing.T, clientset *kubernetes.Clientset) []appsv1.Deployment {
+	deployments, err := clientset.AppsV1().Deployments("default").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		t.Errorf("Cannot retrieve deployments of default namespace: %v", err.Error())
+	}
+	return deployments.Items
+}
+
+func convertDeploymentListToMapOfName(originalDeployments []appsv1.Deployment) map[string]bool {
+	result := make(map[string]bool)
+	for _, d := range originalDeployments {
+		result[d.Name] = true
+	}
+	return result
+}
+
+func getPodsOfDefaultNamespace(t *testing.T, clientset *kubernetes.Clientset) []v1.Pod {
+	pods, err := clientset.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		t.Errorf("Cannot retrieve pods of default namespace: %v", err.Error())
+	}
+	return pods.Items
+}
+
+func convertPodListToMapOfName(originalPods []v1.Pod) map[string]bool {
+	result := make(map[string]bool)
+	for _, p := range originalPods {
+		result[p.Name] = true
+	}
+	return result
+}
+
+func deleteUnitTest(t *testing.T, clientset *kubernetes.Clientset, deploymentName string) map[string]bool {
+	result := make(map[string]bool)
+	originalDeployments := getDeploymentsOfDefaultNamespace(t, clientset)
+	originalPods := getPodsOfDefaultNamespace(t, clientset)
+
+	deleteSampleDeployment(clientset, deploymentName)
+	time.Sleep(90*time.Second)
+
+	deployments := getDeploymentsOfDefaultNamespace(t, clientset)
+	remainingDeployments := convertDeploymentListToMapOfName(deployments)
+	numOfDeletedDeployments := len(originalDeployments) - len(deployments)
+	if numOfDeletedDeployments != 1 {
+		t.Errorf("Want to delete deployment %s, but deleted %d deployments.",
+			deploymentName, numOfDeletedDeployments)
+	}
+	pods := getPodsOfDefaultNamespace(t, clientset)
+	remainingPods := convertPodListToMapOfName(pods)
+	numOfDeletedPods := len(originalPods) - len(pods)
+	if numOfDeletedPods != 4 {
+		t.Errorf("Want to delete pods of deployment %s, but deleted %d pods.", deploymentName, numOfDeletedPods)
+	}
+
+	for _, d := range originalDeployments {
+		if _, ok := remainingDeployments[d.Name]; !ok {
+			t.Logf("Deployment %v is deleted.", d.Name)
+			if d.Name != deploymentName {
+				t.Errorf("Wrongly deleted deployment %v.", d.Name)
+			}
+		}
+	}
+	for _, p := range originalPods {
+		if _, ok := remainingPods[p.Name]; !ok {
+			t.Logf("Deleted pod %v.", p.Name)
+			result[p.Name] = true
+			if !strings.HasPrefix(p.Name, deploymentName) {
+				t.Errorf("The pod %v deleted is not of desired name format", p.Name)
+			}
+		}
+	}
+	return result
+}
+
+func deleteSampleDeployment(clientset *kubernetes.Clientset, deploymentName string) {
+	deleteK8sDeployment(
+		clientset,
+		"default",
+		deploymentName)
+}
