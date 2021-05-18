e0ebe5d9113db09d641c34207a2c30e7cd2f7335
Li Jiayu <jiayu.li@shopee.com>
2021-05-18 12:33:12 +0800

    Handle deployment creation using go client k8s api

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -29,25 +30,31 @@ func handleK8sCommand(reader *bufio.Reader) {
 		clientset := connectToK8s()
 		fmt.Print("Namespace (empty for all): ")
 		namespace := readInput(reader)
-		getPods(clientset, &namespace)
+		getPods(clientset, namespace)
 	} else if task == "create" {
 		clientset := connectToK8s()
 		fmt.Print("Namespace: ")
 		namespace := readInput(reader)
+		fmt.Print("App name: ")
+		appName := readInput(reader)
 		fmt.Print("Deployment name: ")
 		deploymentName := readInput(reader)
+		fmt.Print("Container name: ")
+		containerName := readInput(reader)
 		fmt.Print("Container image: ")
 		image := readInput(reader)
-		launchK8sDeployment(clientset, &namespace, &deploymentName, &image)
+		launchK8sDeployment(clientset, namespace, appName, deploymentName, containerName, image)
 	} else if task == "exit" {
 		os.Exit(0)
+	} else {
+		log.Printf("Invalid task type.")
 	}
 }
 
 func readInput(reader *bufio.Reader) string {
 	result, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Errorf("Cannot read input: %v", err.Error())
+		log.Fatalf("Cannot read input: %v", err.Error())
 	}
 	return result[:(len(result) - 1)]
 }
@@ -73,40 +80,67 @@ func connectToK8s() *kubernetes.Clientset {
 	return clientset
 }
 
-func launchK8sDeployment(clientset *kubernetes.Clientset, namespace *string, deploymentName *string,
-	image *string) {
-	if *namespace == "" {
-		*namespace = "default"
+// https://github.com/kubernetes/client-go/blob/master/examples/create-update-delete-deployment/main.go
+func launchK8sDeployment(
+	clientset *kubernetes.Clientset,
+	namespace string,
+	appName string,
+	deploymentName string,
+	containerName string,
+	image string) {
+	if namespace == "" {
+		namespace = "default"
 	}
+	var numOfReplicas int32 = 4
 
-	deployments := clientset.CoreV1().Pods(*namespace)
+	deploymentsClient := clientset.AppsV1().Deployments(namespace)
 
-	pod := &v1.Pod{
+	deployment := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: *deploymentName,
-			Namespace: *namespace,
+			Name: deploymentName,
 		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
-				{
-					Name: *deploymentName,
-					Image: *image,
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &numOfReplicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": appName,
 				},
 			},
-			RestartPolicy: v1.RestartPolicyNever,
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": appName,
+					},
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: containerName,
+							Image: image,
+							Ports: []v1.ContainerPort{
+								{
+									Name: "http",
+									Protocol: v1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 
-	_, err := deployments.Create(context.TODO(), pod, metav1.CreateOptions{})
+	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
 	if err != nil {
-		log.Fatalf("Failed to create K8s deployment: %v", err.Error())
+		log.Fatalf("Cannot create deployment: %v", err.Error())
 	}
+	log.Printf("Created deployment %v.", result.GetObjectMeta().GetName())
 
-	log.Println("Created K8s deployment.")
 }
 
-func getPods(clientset *kubernetes.Clientset, namespace *string) {
-	if *namespace == "" {
+func getPods(clientset *kubernetes.Clientset, namespace string) {
+	if namespace == "" {
 		namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
 		if err != nil {
 			log.Fatalf("Cannot get list of namespaces: %v", err.Error())
@@ -116,7 +150,7 @@ func getPods(clientset *kubernetes.Clientset, namespace *string) {
 			getPodsOfNamespace(clientset, name)
 		}
 	} else {
-		getPodsOfNamespace(clientset, *namespace)
+		getPodsOfNamespace(clientset, namespace)
 	}
 }
 
