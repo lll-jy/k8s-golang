e5e21eb18b7fff5fadf4fe394daeba8ec88601e8
Li Jiayu <jiayu.li@shopee.com>
2021-05-18 09:38:48 +0800

    Copy function to connect to k8s

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -9,10 +15,41 @@ func main() {
 }
 
 // https://rancher.com/using-kubernetes-api-go-kubecon-2017-session-recap
-func getClient(pathToCfg string) (*kubernetes.Clientset, error) {
+/*func getClient(pathToCfg string) (*kubernetes.Clientset, error) {
 	var config *rest.Confit
 	var err error
 	return nil, nil
-}
+}*/
 
 // https://dev.to/narasimha1997/create-kubernetes-jobs-in-golang-using-k8s-client-go-api-59ej
+func getJobs() {
+	clientset := connectToK8s()
+	jobName := flag.String("jobname", "test-job", "The name of the job")
+	containerImage := flag.String("image", "ubuntu:latest", "Name of the container image")
+	entryCommand := flag.String("command", "ls", "The command to run inside the container")
+
+	flag.Parse()
+
+	fmt.Printf("Args : %s %s %s\n", *jobName, *containerImage, *entryCommand)
+}
+
+func connectToK8s() *kubernetes.Clientset {
+	home, exists := os.LookupEnv("HOME")
+	if !exists {
+		home = "/root"
+	}
+
+	confitPath := filepath.Join(home, ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags("", confitPath)
+	if err != nil {
+		log.Panicln("failed to create K8s config")
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Panicln("Failed to create K8s clientset")
+	}
+
+	return clientset
+}
